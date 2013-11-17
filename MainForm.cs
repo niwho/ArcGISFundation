273db3067de87014705de609b780903a76dc99dc
chenchunliang <chenchunliang1988@126.com>
2013-11-17 14:12:46 +0800

    移除

--- MainForm.cs
+++ MainForm.cs
@@ -341,7 +341,9 @@ namespace ArcGISFoundation
             Size size = new Size(3474,1479);
 
             PrintHelper.ExportActiveView(m_mapControl.ActiveView, size, strImagePath);
-            PrintHelper.ExportActiveView(m_mapControl.ActiveView, 300, 5, strImageType, strImageDir, strImageName, true, m_mapControl.ActiveView.Extent);
+            PrintHelper.ExportActiveView(m_mapControl.ActiveView, 300, 5, 
+                strImageType, strImageDir, strImageName, 
+                true, m_mapControl.ActiveView.Extent);
         }
 
         private void treeView_all_cao_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
