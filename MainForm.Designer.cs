39afe247c6c9300e62c5d77e412e286f45ffac6e
wangshaojun <wangshaojun01@baidu.com>
2013-11-21 22:31:52 +0800

    fe

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -35,15 +35,15 @@ namespace ArcGISFoundation
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("白三叶");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("臂形草");
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("扁穗牛鞭草");
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("紫花苜蓿");
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("所有牧草", new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode2,
-            treeNode3,
-            treeNode4});
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("白三叶");
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("臂形草");
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("扁穗牛鞭草");
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("紫花苜蓿");
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("所有牧草", new System.Windows.Forms.TreeNode[] {
+            treeNode11,
+            treeNode12,
+            treeNode13,
+            treeNode14});
             this.licensecontrol = new ESRI.ArcGIS.Controls.AxLicenseControl();
             this.toccontrol = new ESRI.ArcGIS.Controls.AxTOCControl();
             this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
@@ -75,6 +75,7 @@ namespace ArcGISFoundation
             this.min = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.licensecontrol)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.toccontrol)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
@@ -247,18 +248,18 @@ namespace ArcGISFoundation
             this.treeView_all_cao.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(147)))), ((int)(((byte)(207)))));
             this.treeView_all_cao.Location = new System.Drawing.Point(0, 25);
             this.treeView_all_cao.Name = "treeView_all_cao";
-            treeNode1.Name = "节点1";
-            treeNode1.Text = "白三叶";
-            treeNode2.Name = "节点2";
-            treeNode2.Text = "臂形草";
-            treeNode3.Name = "节点3";
-            treeNode3.Text = "扁穗牛鞭草";
-            treeNode4.Name = "节点4";
-            treeNode4.Text = "紫花苜蓿";
-            treeNode5.Name = "节点0";
-            treeNode5.Text = "所有牧草";
+            treeNode11.Name = "节点1";
+            treeNode11.Text = "白三叶";
+            treeNode12.Name = "节点2";
+            treeNode12.Text = "臂形草";
+            treeNode13.Name = "节点3";
+            treeNode13.Text = "扁穗牛鞭草";
+            treeNode14.Name = "节点4";
+            treeNode14.Text = "紫花苜蓿";
+            treeNode15.Name = "节点0";
+            treeNode15.Text = "所有牧草";
             this.treeView_all_cao.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode5});
+            treeNode15});
             this.treeView_all_cao.Size = new System.Drawing.Size(198, 400);
             this.treeView_all_cao.TabIndex = 0;
             this.treeView_all_cao.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView_all_cao_NodeMouseDoubleClick);
@@ -291,6 +292,7 @@ namespace ArcGISFoundation
             // 
             // panel_toolbar
             // 
+            this.panel_toolbar.Controls.Add(this.label3);
             this.panel_toolbar.Controls.Add(this.m_LayerList);
             this.panel_toolbar.Controls.Add(this.maintoolbar);
             this.panel_toolbar.Dock = System.Windows.Forms.DockStyle.Top;
@@ -305,7 +307,7 @@ namespace ArcGISFoundation
             this.m_LayerList.FormattingEnabled = true;
             this.m_LayerList.IntegralHeight = false;
             this.m_LayerList.ItemHeight = 17;
-            this.m_LayerList.Location = new System.Drawing.Point(249, 1);
+            this.m_LayerList.Location = new System.Drawing.Point(350, 1);
             this.m_LayerList.Name = "m_LayerList";
             this.m_LayerList.Size = new System.Drawing.Size(115, 25);
             this.m_LayerList.TabIndex = 13;
@@ -530,6 +532,16 @@ namespace ArcGISFoundation
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(264, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "指定查询图层:";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -557,6 +569,7 @@ namespace ArcGISFoundation
             this.panel_container.ResumeLayout(false);
             this.panel_right_map.ResumeLayout(false);
             this.panel_toolbar.ResumeLayout(false);
+            this.panel_toolbar.PerformLayout();
             this.panel_left.ResumeLayout(false);
             this.panel_title_bar.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
@@ -609,6 +622,7 @@ namespace ArcGISFoundation
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.SaveFileDialog saveFileDialog_printer;
         private System.Windows.Forms.ComboBox m_LayerList;
+        private System.Windows.Forms.Label label3;
     }
 }
 
